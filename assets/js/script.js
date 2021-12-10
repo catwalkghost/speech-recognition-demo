e1630fde5b129dd857b8cd36dd7bd93b4d8e7f45
Alex Unku <alex.unku@miceanddice.com>
2021-12-10 12:47:31 +0000

    changed const to var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
 function testSpeech() {
 
-    const diagnostic = document.querySelector('.output');
-    const recognition = new SpeechRecognition();
+    var diagnostic = document.querySelector('.output');
+    var recognition = new SpeechRecognition();
     recognition.interimResults = true;
 
     recognition.start();
 
     recognition.onresult = function(event) {
-        const speechResult = event.results[0][0].transcript;
+        var speechResult = event.results[0][0].transcript;
         diagnostic.textContent = speechResult;
     }
 
